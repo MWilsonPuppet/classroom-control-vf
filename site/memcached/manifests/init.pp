d3810bacb827ab3497048546e7913f95c0bc9c89
MWilsonPuppet <mark.wilson@puppetlabs.com>
2016-02-23 13:02:49 +0000

    Create init.pp

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,23 @@
+class memcached{
+
+  package{'memcached':
+    ensure  => present,
+    before  => File['/etc/sysconfig/memcached'],
+  }
+  
+  file{ '/etc/sysconfig/memcached':
+    ensure  =>  'file',
+    source  => 'puppet:///modules/memcached/sysconfig/memcached',
+    require => Package['memcached'],
+    notify  => Service['memcached'],
+  }
+
+  service{ 'memcached':
+    ensure    => 'running',
+    subscribe =>  File['/etc/sysconfig/memcached'],
+  }
+
+
+
+
+}
